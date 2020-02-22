1ddd016564276492c397e590d2dc32ce5b26604c
Arkanoid <rudo1ntandoyenkosi@gmail.com>
2020-02-22 11:28:20 +0200

    Program commit

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,36 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public static string Prefix(string input)
+        {
+            int inp = input.Length;
+            string[] wrds = input.Split(" ");
+            int numWords = wrds.Length;
+            int lenword = 0;
+            foreach(string i in wrds)
+            {
+                if(i != "")
+                {
+                    lenword ++;
+                }
+            }
+
+            if(input ==""){
+                numWords= 0;
+            }
+        
+
+            string ans =inp + ","+ lenword + ":" + input;
+            return ans;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = "hello";
+           Prefix(name);
+            
         }
     }
 }
